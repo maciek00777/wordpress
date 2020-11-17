133a45bac530da26c3077fe33ae9d334a8139ea9
Maciej Kopeć <maciej.kopec@szkola.salezjanie.com>
2020-11-17 11:02:07 +0100

    init2

---
+++ readme.md
@@ -0,0 +1,7 @@
+Jakby czegoś brakowało, były jakieś problemy, proszę pisać
+
+U mnie działa i wygląda to tak:
+![little](img/mobile.png)
+![bigger](img/bigger.png)
+![archiwa1](img/archiwa2.png)
+![archiwa2](img/archiwa.png)
--- wp-content/themes/test/archive.php
+++ wp-content/themes/test/archive.php
@@ -4,9 +4,8 @@
 if ( have_posts() ) : 
     while ( have_posts() ) : the_post();
 ?>
-    <article>
-
-        <span> <?php the_time();?></span>
+    <article style="border: 1px; border-style:solid">
+        <span> <?php the_time();?> <?php echo get_the_date() ?></span>
         <h2>
             <?php the_title(); ?>
         </h2>
